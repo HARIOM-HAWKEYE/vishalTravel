e54615933d3a705c022440e924098dd31d2836c1
hariomSinghRaghuvanshi <hawkeyesingh09@gmail.com.>
2025-01-01 15:54:44 +0530

    some updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
-import { NavComponent } from '../component/navbar/navbar'
-import { MainContent } from '../component/main-page/MainContent'
-import { Milestone } from '../component/countingboxes/milestone'
-import { BestLocation } from '../component/locations/locations'
-import { Benefit } from '../component/benefit/benefit'
-import { Carousel } from '../component/carousel/carousel'
+import { NavComponent } from '../component/navbar/navbar.jsx'
+import { MainContent } from '../component/main-page/MainContent.jsx'
+import { Milestone } from '../component/countingboxes/milestone.jsx'
+import { BestLocation } from '../component/locations/locations.jsx'
+import { Benefit } from '../component/benefit/benefit.jsx'
+import { Carousel } from '../component/carousel/carousel.jsx'
 function App() {
   const [count, setCount] = useState(0)
  const Slides =[{image:"https://uttarakhandtourism.gov.in/sites/default/files/2022-05/Banner_Kartik_Swami_Temple.jpeg"},{image:"https://media.istockphoto.com/id/484389570/photo/himalayas.jpg?s=612x612&w=0&k=20&c=6jM32XJBWlvv6x7AF-av52jlncPkz5hn_AEhjaDSFYI="},{image:"https://media.istockphoto.com/id/484389570/photo/himalayas.jpg?s=612x612&w=0&k=20&c=6jM32XJBWlvv6x7AF-av52jlncPkz5hn_AEhjaDSFYI="},
