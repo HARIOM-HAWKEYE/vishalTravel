6bbc1324881aec291c81230d6916194626d5b709
hariomSinghRaghuvanshi <hawkeyesingh09@gmail.com.>
2025-01-01 16:32:04 +0530

    some updates

--- component/locations/locations.jsx
+++ component/locations/locations.jsx
@@ -1,6 +1,7 @@
 import "./locations.scss";
 import { IoLocationSharp } from "react-icons/io5";
 import { Heading } from "../heading/heading";
+import uttrakhand  from "../../src/assets/locations/uttrakhand.jpg";
 export function BestLocation(){
     return <>
        <Heading biggerHeading="Incredible India" smallerHeading="Best Location" >Extraordinary natural beauty,enjoy the rich culture,<br /> and experience the friendliness of the local people.</Heading>
@@ -8,11 +9,11 @@ export function BestLocation(){
              
            
               <div id="gallery-grid">
-                <div class="item item-1"><span className="PlaceName">Kedarnath Temple <br /> <span className="city"><IoLocationSharp />Uttrakhand</span></span></div>
-                <div class="item item-2"><span className="PlaceName">Kedarnath Temple <br /> <span className="city"><IoLocationSharp />Uttrakhand</span></span></div>
-                <div class="item item-3"><span className="PlaceName">Kedarnath Temple <br /> <span className="city"><IoLocationSharp />Uttrakhand</span></span></div>
-                <div class="item item-4"><span className="PlaceName">Kedarnath Temple <br /> <span className="city"><IoLocationSharp />Uttrakhand</span></span></div>
-                <div class="item item-5"><span className="PlaceName">Kedarnath Temple <br /> <span className="city"><IoLocationSharp />Uttrakhand</span></span></div>
+                <div class="item item-1"style={{backgroundImage: `url(${uttrakhand})`}} ><span className="PlaceName">Kedarnath Temple <br /> <span className="city"><IoLocationSharp />Uttrakhand</span></span></div>
+                <div class="item item-2"style={{backgroundImage: `url(${uttrakhand})`}}  ><span className="PlaceName">Kedarnath Temple <br /> <span className="city"><IoLocationSharp />Uttrakhand</span></span></div>
+                <div class="item item-3"style={{backgroundImage: `url(${uttrakhand})`}} ><span className="PlaceName">Kedarnath Temple <br /> <span className="city"><IoLocationSharp />Uttrakhand</span></span></div>
+                <div class="item item-4"style={{backgroundImage: `url(${uttrakhand})`}} ><span className="PlaceName">Kedarnath Temple <br /> <span className="city"><IoLocationSharp />Uttrakhand</span></span></div>
+                <div class="item item-5"style={{backgroundImage: `url(${uttrakhand})`}}><span className="PlaceName">Kedarnath Temple <br /> <span className="city"><IoLocationSharp />Uttrakhand</span></span></div>
               </div>
 
 
